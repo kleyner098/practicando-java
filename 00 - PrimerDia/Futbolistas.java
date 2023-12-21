b2c8ffbd8386f45fd9d162e78905c313b6926a79
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2023-12-21 17:49:12 +0100

    first commit

---
+++ 00 - PrimerDia/Futbolistas.java
@@ -0,0 +1,37 @@
+class Futbolistas {
+    public static void main(String[] args) {
+       
+        // Ejemplo arrays + switch con cadenas
+        String[] jugador =  {"delantero","medio","defensa","portero","entrenador"};
+        
+        System.out.println(jugador[5]);
+        
+
+        /*for (int i=0;i<jugador.length;i++){
+            switch (jugador[i]) {
+                case "delantero":
+                    System.out.println("El delantero chuta");
+                    break;
+
+                case "medio":
+                    System.out.println("El medio pasa el balon");
+                    break;
+
+                case "defensa":
+                case "portero":
+                    System.out.println("Defienden gol");
+                    break;
+        
+                default:
+                    System.out.println("Hola, entrenador");
+                    break;
+            }
+        }*/
+
+    }
+
+         
+
+
+
+    }
---
+++ 00 - PrimerDia/Switch.java
@@ -0,0 +1,29 @@
+class Switch {
+    public static void main(String[] args) {
+       
+
+        int entero = 0;
+
+        switch (entero) {
+            case 0:
+                System.out.println("Hola Soy un cero");
+
+            case 1:
+                System.out.println("Hola Soy un uno");
+                break;
+
+            case 2:
+                System.out.println("Hola Soy un dos");
+                break;
+        
+            default:
+                System.out.println("Hola");
+                break;
+        }
+    }
+
+         
+
+
+
+    }
