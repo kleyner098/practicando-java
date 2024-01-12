ae999567b561ec7e1a97b19b3740be54d563ba8d
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2024-01-12 13:14:11 +0100

    Codigo Feedback Bisiestos

--- 06 - Arrays/FeedbackBisiestos.java
+++ 06 - Arrays/FeedbackBisiestos.java
@@ -5,8 +5,12 @@ public class FeedbackBisiestos {
         Scanner sc = new Scanner(System.in);
         int año = sc.nextInt();
 
-        boolean esBisiesto = (año % 4 == 0) && ((año % 100 != 0) || ((año % 100 == 0)&&(año % 400 ==0)));
-        System.out.println(esBisiesto?"Es un año bisiesto":"No es un año bisiesto");
+        /*boolean esBisiesto = (año % 4 == 0) && ((año % 100 != 0) || ((año % 100 == 0)&&(año % 400 ==0)));
+        System.out.println(esBisiesto?"Es un año bisiesto":"No es un año bisiesto");*/
+
+        System.out.println((año % 4 == 0) && ((año % 100 != 0) || 
+         ((año % 100 == 0)&&(año % 400 ==0)))?"Es un año bisiesto":"No es un año bisiesto");
+         
         sc.close();
     }
 }
