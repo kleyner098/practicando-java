7bd419765a30f34010f079b69602a627cc64e8b3
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2024-01-15 16:37:24 +0100

    Quitar mensaje de prueba

--- 07 - ConexionMySQL/ConectarSQL.java
+++ 07 - ConexionMySQL/ConectarSQL.java
@@ -2,7 +2,7 @@
 
 public class ConectarSQL {
     public static void main(String[] args) {
-        System.out.println("Hola");
+    
         ConexionMySQL cx = new ConexionMySQL();
         cx.conectarMySQL();
     }
