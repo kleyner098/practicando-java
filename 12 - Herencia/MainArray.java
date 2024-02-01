78417816b54f7b7a9d376daf2176c27b2369869b
BegoRodriguez <136585766+BegoRodriguez@users.noreply.github.com>
2024-02-01 18:59:38 +0100

    cambiado array a mayusculas

--- 12 - Herencia/MainArray.java
+++ 12 - Herencia/MainArray.java
@@ -3,11 +3,11 @@ public class MainArray {
 
         /* Modelado de las efectividades en el ejercicio de Pokemon */
         /* La estructura para declarar un Array multidimensional es la siguiente: int [filas][columnas]   */
-        double[][] efectividad = {{0.5, 0.5, 2, 2}, {2, 0.5, 0.5, 1}, {0.5, 2, 0.5, 0.5}, {1, 1, 1, 0.5}};
+        final double[][] EFECTIVIDAD = {{0.5, 0.5, 2, 2}, {2, 0.5, 0.5, 1}, {0.5, 2, 0.5, 0.5}, {1, 1, 1, 0.5}};
         /* Agua = 0; Fuego = 1; Planta = 2; Electrico = 3 */
         /* Primero defensor; Segundo atacante */
-        System.out.println("" + efectividad[0][0]);
-        System.out.println("" + efectividad[0][2]);
+        System.out.println("" + EFECTIVIDAD[0][0]);
+        System.out.println("" + EFECTIVIDAD[0][2]);
     }
     
 }
